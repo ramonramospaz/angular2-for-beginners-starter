d21e1e158e274fc2e1a6fe409119beb82662d313
Ramón Antonio Ramos Paz <ramonramospaz@gmail.com>
2016-09-07 15:05:24 -0400

    Update to the new template of RC5
    It a new basic template of the RC5.

--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,28 @@
 
 import {Component} from "@angular/core";
-import {bootstrap} from "@angular/platform-browser-dynamic";
+import {NgModule} from "@angular/core";
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic";
+import {BrowserModule} from "@angular/platform-browser";
+
 
 @Component({
     selector:'my-app',
     template: `<h1>Hello World !</h1>`
 })
-export class App {
-
+export class HelloWorld {
 
 
 }
 
-bootstrap(App);
+
+@NgModule({
+    declarations: [HelloWorld],
+    imports: [BrowserModule],
+    bootstrap: [HelloWorld]
+})
+export class AppModule {
+
+}
+
+
+platformBrowserDynamic().bootstrapModule(AppModule);
